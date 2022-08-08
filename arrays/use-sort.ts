853a192300f9dd4f668491e52deafe06467d9faa
arysev <rysev-a@yandex.ru>
2022-08-08 17:49:59 +0300

    Init project

---
+++ arrays/use-sort.ts
@@ -0,0 +1,56 @@
+import { User } from './__test__/users-data';
+
+// Отсортировать пользователей по имени
+
+export const sortUsersByName = (users: User[]) => {
+  users.sort((userA: User, userB: User) => {
+    if (userA.name > userB.name) {
+      return 1;
+    }
+
+    return -1;
+  });
+
+  return users;
+};
+
+// Отсортировать пользователей по опыту по возрастанию
+export const sortUsersByExperienceASC = (users: User[]) => {
+  users.sort((userA: User, userB: User) => {
+    if (userA.experience > userB.experience) {
+      return 1;
+    }
+
+    return -1;
+  });
+
+  return users;
+};
+
+// Отсортировать пользователей по опыту по убыванию
+export const sortUsersByExperienceDESC = (users: User[]) => {
+  users.sort((userA: User, userB: User) => {
+    if (userA.experience > userB.experience) {
+      return -1;
+    }
+
+    return 1;
+  });
+
+  return users;
+};
+
+// Отсортировать пользователей по длине имени
+export const sortUsersByNameLength = (users: User[]) => {
+  sortUsersByExperienceDESC(users);
+
+  users.sort((userA: User, userB: User) => {
+    if (userA.name.length > userB.name.length) {
+      return 1;
+    }
+
+    return -1;
+  });
+
+  return users;
+};
